fe0fcd7586026a1599e517b4dccba3bc8e0da3cb
Priji P <priji.p@gmail.com>
2019-10-31 06:54:52 +0530

    WIP - semantic-release

---
+++ semantic-release/gen-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Generate Build Script"
+echo "nextRelease.version: $1"
+echo "options.branch: $2"
+echo "commits.length: $3"
+echo "build-date: $4"
+
+go install -ldflags "-X main.Version=$1 -X main.BuildDate=$4" github.com/prijip/gofind/cmd/gofind
+
+gofind --version
